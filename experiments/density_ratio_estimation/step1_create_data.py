c0fbbb70c7ef10b7abf9dbb4b365ce5a09b13983
Chandler Squires <chandlersquires18@gmail.com>
2026-01-22 13:13:14 -0500

    data creation script

--- experiments/density_ratio_estimation/step1_create_data.py
+++ experiments/density_ratio_estimation/step1_create_data.py
@@ -1,6 +1,24 @@
+import os
+import pickle
+
+from scipy.stats import multivariate_normal
+
+from experiments.utils.two_gaussians_kl import create_two_gaussians_kl_range
 
 
-# TODO: for each KL distance, create parameters for p0 and p1 such that KL(p0 || p1) = KL_distance
-# TODO: create samples from p0 and p1
-# TODO: save samples to data/density_ratio_estimation/kl_distance_p0_p1.pkl
+KL_DISTANCES = [0.5, 1, 2, 4, 8, 16, 32, 64, 128, 256]
+NSAMPLES = 1000
 
+DATA_DIR = 'experiments/density_ratio_estimation/data'
+os.makedirs(DATA_DIR, exist_ok=True)
+for kl_distance in KL_DISTANCES:
+    gaussian_pairs = create_two_gaussians_kl_range(dim=3, k=kl_distance, beta_min=0.3, beta_max=0.7, npairs=100)
+    data = []
+    for gaussian_pair in gaussian_pairs:
+        p0 = multivariate_normal(gaussian_pair['mu0'], gaussian_pair['Sigma0'])
+        p1 = multivariate_normal(gaussian_pair['mu1'], gaussian_pair['Sigma1'])
+        samples_p0 = p0.rvs(NSAMPLES)
+        samples_p1 = p1.rvs(NSAMPLES)
+        data.append(dict(samples_p0=samples_p0, samples_p1=samples_p1))
+
+    pickle.dump(data, open(f'{DATA_DIR}/k={kl_distance}.pkl', 'wb'))
