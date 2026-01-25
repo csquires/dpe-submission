688ee51f1de17d252c89a2b514a992c5f9a5c017
csquires <chandlersquires18@gmail.com>
2026-01-25 13:49:03 -0500

    start editing step2_run_algorithms

--- experiments/density_ratio_estimation/step2_run_algorithms.py
+++ experiments/density_ratio_estimation/step2_run_algorithms.py
@@ -35,20 +35,20 @@ algorithms = [
 ]
 
 os.makedirs(RAW_RESULTS_DIR, exist_ok=True)
-with h5py.File(f'{DATA_DIR}/dataset_d={DATA_DIM},ntrain={NSAMPLES_TRAIN},ntest={NSAMPLES_TEST}.h5', 'r') as f:
-    nrows = f['kl_distance_arr'].shape[0]
+with h5py.File(f'{DATA_DIR}/dataset_d={DATA_DIM},ntrain={NSAMPLES_TRAIN},ntest={NSAMPLES_TEST}.h5', 'r') as dataset_file:
+    nrows = dataset_file['kl_distance_arr'].shape[0]
 
     for alg_name, alg in algorithms:
         est_ldrs_arr = np.zeros((nrows, NTEST_SETS, NSAMPLES_TEST))
         for idx in trange(nrows):
-            samples_p0 = torch.from_numpy(f['samples_p0_arr'][idx]).to(DEVICE)  # (NSAMPLES_TRAIN, DATA_DIM)
-            samples_p1 = torch.from_numpy(f['samples_p1_arr'][idx]).to(DEVICE)  # (NSAMPLES_TRAIN, DATA_DIM)
+            samples_p0 = torch.from_numpy(dataset_file['samples_p0_arr'][idx]).to(DEVICE)  # (NSAMPLES_TRAIN, DATA_DIM)
+            samples_p1 = torch.from_numpy(dataset_file['samples_p1_arr'][idx]).to(DEVICE)  # (NSAMPLES_TRAIN, DATA_DIM)
             alg.fit(samples_p0, samples_p1)
 
-            samples_pstar = torch.from_numpy(f['samples_pstar_arr'][idx]).to(DEVICE)  # (NTEST_SETS, NSAMPLES_TEST, DATA_DIM)
+            samples_pstar = torch.from_numpy(dataset_file['samples_pstar_arr'][idx]).to(DEVICE)  # (NTEST_SETS, NSAMPLES_TEST, DATA_DIM)
             for test_set_idx in range(NTEST_SETS):
                 est_ldrs = alg.predict_ldr(samples_pstar[test_set_idx])  # (NSAMPLES_TEST,)
                 est_ldrs_arr[idx, test_set_idx] = est_ldrs.cpu().numpy()
 
-        with h5py.File(f'{RAW_RESULTS_DIR}/results_d={DATA_DIM},ntrain={NSAMPLES_TRAIN},ntest={NSAMPLES_TEST}.h5', 'a') as f:
-            f.create_dataset(f'est_ldrs_arr_{alg_name}', data=est_ldrs_arr)
+        with h5py.File(f'{RAW_RESULTS_DIR}/results_d={DATA_DIM},ntrain={NSAMPLES_TRAIN},ntest={NSAMPLES_TEST}.h5', 'a') as results_file:
+            results_file.create_dataset(f'est_ldrs_arr_{alg_name}', data=est_ldrs_arr)
