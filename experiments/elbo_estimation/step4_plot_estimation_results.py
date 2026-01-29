c2cd207b0f7b3c4ef6ffd727b49c6365d7520702
YizhouLu_Johnson <yizhoulu51@gmail.com>
2026-01-29 01:43:30 -0500

    plot configurations

--- experiments/elbo_estimation/step4_plot_estimation_results.py
+++ experiments/elbo_estimation/step4_plot_estimation_results.py
@@ -18,28 +18,64 @@ FIGURES_DIR = config['figures_dir']
 ALPHAS = config['alphas']
 DESIGN_EIG_PERCENTAGES = config['design_eig_percentages']
 
-processed_results_filename = f'{PROCESSED_RESULTS_DIR}/errors_d={DATA_DIM},nsamples={NSAMPLES}.h5'
-
+#processed_results_filename = f'{PROCESSED_RESULTS_DIR}/errors_d={DATA_DIM},nsamples={NSAMPLES}.h5'
+processed_results_filename = f'{PROCESSED_RESULTS_DIR}/complete_elbo.h5'
 # Load processed results
 with h5py.File(processed_results_filename, 'r') as f:
     # Find all algorithms
     alg_names = [key.replace('mae_', '') for key in f.keys() if key.startswith('mae_')]
     mae_by_alg = {alg_name: f[f'mae_{alg_name}'][:] for alg_name in alg_names}
 
+colors = {
+    "BDRE": "#1f77b4",
+    "TDRE": "#ff7f0e",
+    "MDRE": "#2ca02c",
+    "TSM": "#d62728",
+    "TriangularMDRE": "#9467bd",
+    "VFM": "#9467bd",
+    "Direct3": "#8c564b",
+    "Direct4": "#e377c2",
+    "Direct5": "#7f7f7f",
+}
+legend_order = ["BDRE", "TDRE", "MDRE", "TSM", "TriangularMDRE", "VFM", "Direct3", "Direct4", "Direct5"]
+
 # Plot
 plt.clf()
 fig, axes = plt.subplots(figsize=(10, 3), nrows=1, ncols=len(ALPHAS))
+all_vals = []
+for mae in mae_by_alg.values():
+    all_vals.extend(mae.flatten().tolist())
+if all_vals:
+    y_min, y_max = min(all_vals), max(all_vals)
+else:
+    y_min, y_max = 0.0, 1.0
+
 for i, alpha in enumerate(ALPHAS):
-    for alg_name, mae in mae_by_alg.items():
-        # mae shape: (len(DESIGN_EIG_PERCENTAGES), len(ALPHAS))
-        axes[i].plot(DESIGN_EIG_PERCENTAGES, mae[:, i], label=alg_name)
+    # stable legend order
+    alg_names = sorted(mae_by_alg.keys(), key=lambda n: legend_order.index(n) if n in legend_order else len(legend_order))
+    for alg_name in alg_names:
+        mae = mae_by_alg[alg_name]
+        axes[i].plot(
+            DESIGN_EIG_PERCENTAGES,
+            mae[:, i],
+            label=alg_name,
+            color=colors.get(alg_name, "#333333"),
+            linewidth=1.0,
+        )
     axes[i].set_title(fr"$\alpha = {alpha}$")
+    axes[i].set_ylim(y_min * 0.9, y_max * 1.1)
+    if i != 0:
+        axes[i].set_ylabel("")
+        axes[i].tick_params(axis="y", labelleft=False)
 
 fig.supxlabel(r"$\beta$ (Design Optimality Percentage)")
 fig.supylabel(r"ELBO Estimation Error")
 handles, labels = axes[0].get_legend_handles_labels()
-plt.legend(handles, labels, bbox_to_anchor=(1.05, 1), loc='upper left')
+by_label = dict(zip(labels, handles))
+ordered_labels = [lbl for lbl in legend_order if lbl in by_label]
+ordered_labels += [lbl for lbl in by_label.keys() if lbl not in ordered_labels]
+plt.legend([by_label[lbl] for lbl in ordered_labels], ordered_labels, bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 # saving
 os.makedirs(FIGURES_DIR, exist_ok=True)
-plt.savefig(f'{FIGURES_DIR}/elbo_estimation.pdf')
+plt.savefig(f'{FIGURES_DIR}/elbo_estimation_complete.pdf')
